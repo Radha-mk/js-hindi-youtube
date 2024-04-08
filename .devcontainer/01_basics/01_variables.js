945addcd9a9d4d92e20debbdcae763740d07c743
Radha-mk <75665570+Radha-mk@users.noreply.github.com>
2024-04-08 09:57:01 +0000

    Variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 14453
+let accountEmail = "radha@google.com"
+var accountpassword = "12345"
+accountCity = "Jaipur"
+
+accountEmail = "frugrf"
+
+accountpassword = "23345"
+
+accountCity = "pune"
+// accountId = 2 //not allowed
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountpassword,accountCity,accountState])
+
+/*
+
+prefer not to use var:because of issue in block scope and functional scope.
+*/
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all code as newer version 
+//alert ("hello") we are using node JS not browser.
+console.log(3+3);console.log("Radha") //code readability should be high.
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+//number => 2 to the power of 53
+//nigInt 
+//string = ""
+//boolean = true or false
+//null = standalone value
+//undefined =>
+//symbol = unique 
+
+//object 
+console.log (typeof null);//object
+console.log(typeof undefined);//undefined
+
+
+
